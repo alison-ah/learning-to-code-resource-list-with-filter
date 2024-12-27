0ee9a7d8fb13bac5bdcd6965d5bd7c6fdf92225c
Alison <alison@andersonholland.com>
2024-12-27 14:34:47 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -41,35 +41,6 @@ export const resources = [
   },
   {
     id: 3,
-    name: "21 Easy JavaScript Projects for Beginners (Code included!)",
-    url: "https://skillcrush.com/blog/projects-you-can-do-with-javascript/",
-    imageSrc: "/img/project.png",
-    alt: "project",
-    html: false,
-    css: false,
-    accessibility: false,
-    javaScript: true,
-    react: false,
-    python: false,
-    dataViz: false,
-    skillcrush: true,
-  },
-  {
-    id: 4,
-    name: "Flexbook Zombie",
-    url: "https://flexboxzombies.com/p/flexbox-zombies",
-    imageSrc: "/img/game.png",
-    alt: "game",
-    html: false,
-    css: true,
-    javaScript: false,
-    react: false,
-    python: false,
-    dataViz: false,
-    skillcrush: false,
-  },
-  {
-    id: 5,
     name: "How to check your web accessibility for free",
     url: "https://silktide.com/blog/how-to-check-your-web-accessibility-for-free/",
     imageSrc: "/img/blog_or_docs.png",
@@ -84,7 +55,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 6,
+    id: 4,
     name: "Real Talk on Web Accessibility with Marcy Sutton Todd",
     url: "https://syntax.fm/show/836/real-talk-on-web-accessibility-with-marcy-sutton-todd",
     imageSrc: "/img/podcast.png",
@@ -99,7 +70,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 7,
+    id: 5,
     name: "Frontend Mentor",
     url: "https://www.frontendmentor.io/",
     imageSrc: "/img/project.png",
@@ -114,7 +85,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 8,
+    id: 6,
     name: "Python for Web Apps and Data",
     url: "https://skillcrush.com/blueprint/python-web-apps-data-course/",
     imageSrc: "/img/course.png",
@@ -129,7 +100,7 @@ export const resources = [
     skillcrush: true,
   },
   {
-    id: 9,
+    id: 7,
     name: "JavaScript 30",
     url: "https://javascript30.com/",
     imageSrc: "/img/course.png",
@@ -144,7 +115,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 10,
+    id: 8,
     name: "CSS Flexbook Layout Guide",
     url: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
     imageSrc: "/img/blog_or_docs.png",
@@ -159,7 +130,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 11,
+    id: 9,
     name: "Wes Bos' Course on Flexbox",
     url: "https://flexbox.io/",
     imageSrc: "/img/course.png",
@@ -174,7 +145,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 12,
+    id: 10,
     name: "An Interactive Guide to Flexbox",
     url: "https://www.joshwcomeau.com/css/interactive-guide-to-flexbox/",
     imageSrc: "/img/blog_or_docs.png",
@@ -189,7 +160,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 13,
+    id: 11,
     name: "Chrome DevTools and Flexbox Docs",
     url: "https://developer.chrome.com/docs/devtools/css/",
     imageSrc: "/img/blog_or_docs.png",
@@ -204,7 +175,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 14,
+    id: 12,
     name: "Flexbox Playground",
     url: "https://codepen.io/enxaneta/full/adLPwv/",
     imageSrc: "/img/blog_or_docs.png",
@@ -219,7 +190,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 15,
+    id: 13,
     name: "devChallenges",
     url: "https://devchallenges.io/",
     imageSrc: "/img/project.png",
@@ -234,7 +205,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 16,
+    id: 14,
     name: "iCodeThis",
     url: "https://icodethis.com/",
     imageSrc: "/img/project.png",
@@ -249,7 +220,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 17,
+    id: 15,
     name: "Codier",
     url: "https://codier.io/",
     imageSrc: "/img/project.png",
@@ -264,7 +235,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 18,
+    id: 16,
     name: "Chingu",
     url: "https://www.chingu.io/",
     imageSrc: "/img/project.png",
@@ -279,7 +250,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 19,
+    id: 17,
     name: "React Docs",
     url: "https://react.dev/",
     imageSrc: "/img/blog_or_docs.png",
@@ -294,7 +265,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 20,
+    id: 18,
     name: "React Visualized",
     url: "https://react.gg/visualized",
     imageSrc: "/img/blog_or_docs.png",
@@ -309,7 +280,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 21,
+    id: 19,
     name: "Accessible U",
     url: "https://accessibility.umn.edu/creating-accessible-content/develop-accessible-websites-applications",
     imageSrc: "/img/blog_or_docs.png",
@@ -324,7 +295,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 22,
+    id: 20,
     name: "allyphant",
     url: "https://a11yphant.com/",
     imageSrc: "/img/course.png",
@@ -339,7 +310,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 23,
+    id: 21,
     name: "Evergreen Data",
     url: "https://stephanieevergreen.com/blog/",
     imageSrc: "/img/blog_or_docs.png",
@@ -354,7 +325,7 @@ export const resources = [
     skillcrush: false,
   },
   {
-    id: 24,
+    id: 22,
     name: "Front End Development",
     url: "https://skillcrush.com/blueprint/front-end-development-course/",
     imageSrc: "/img/course.png",
@@ -368,5 +339,35 @@ export const resources = [
     dataViz: false,
     skillcrush: true,
   },
+  {
+    id: 23,
+    name: "21 Easy JavaScript Projects for Beginners (Code included!)",
+    url: "https://skillcrush.com/blog/projects-you-can-do-with-javascript/",
+    imageSrc: "/img/project.png",
+    alt: "project",
+    html: false,
+    css: false,
+    accessibility: false,
+    javaScript: true,
+    react: false,
+    python: false,
+    dataViz: false,
+    skillcrush: true,
+  },
+  {
+    id: 24,
+    name: "Flexbook Zombie",
+    url: "https://flexboxzombies.com/p/flexbox-zombies",
+    imageSrc: "/img/game.png",
+    alt: "game",
+    html: false,
+    css: true,
+    javaScript: false,
+    react: false,
+    python: false,
+    dataViz: false,
+    skillcrush: false,
+  },
+
 
 ];
